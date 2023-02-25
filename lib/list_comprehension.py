9e68b11e97801b2b77c03f27d47ef6bbb27b9a2f
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-25 15:07:01 -0500

    exclamation

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -4,4 +4,4 @@ def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
